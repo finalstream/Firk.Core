6ad42b8b56fe6d078254d4f8628a70c8482b7949
finalstream <info@finalstream.net>
2015-12-29 23:37:43 +0900

    add assemblyinfo

---
+++ source/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+
+[assembly: AssemblyTitle("Firk Framework Core")]
+[assembly: AssemblyDescription("Finalstream App Framework")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Finalstream")]
+[assembly: AssemblyProduct("Firk Framework Core")]
+[assembly: AssemblyCopyright("Copyright ©  2015 Finalstream")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから 
+// 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、
+// その型の ComVisible 属性を true に設定してください。
+
+[assembly: ComVisible(false)]
+
+// 次の GUID は、このプロジェクトが COM に公開される場合の、typelib の ID です
+
+[assembly: Guid("dbd81f6b-6985-48c0-94da-e1592c5c0f4c")]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
+// 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
